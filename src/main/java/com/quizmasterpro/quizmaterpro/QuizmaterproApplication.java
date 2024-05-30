909acdb086f7f7df23584f36f40254f6e146364e
Rushi_0508 <rushigandhi14@gmail.com>
2024-05-30 11:21:52 +0530

    Initial Configuration

---
+++ src/main/java/com/quizmasterpro/quizmaterpro/QuizmaterproApplication.java
@@ -0,0 +1,13 @@
+package com.quizmasterpro.quizmaterpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmaterproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmaterproApplication.class, args);
+	}
+
+}
