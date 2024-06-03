cf7d2bdc0ae8c70762c629020e81a77b3874c63d
Rushi_0508 <rushigandhi14@gmail.com>
2024-06-03 19:16:49 +0530

    Removed Unwanted Imports

--- src/main/java/com/quizmasterpro/quizmaterpro/Controllers/QuizController.java
+++ src/main/java/com/quizmasterpro/quizmaterpro/Controllers/QuizController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
